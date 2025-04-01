bd73174e936dea4d4b36e7bcb6c12d1fd97f1807
Stv-devl <lansadstevan@gmail.com>
2025-04-01 14:57:48 +0700

    fix prisma for google connnection

--- lib/config.ts
+++ lib/config.ts
@@ -3,6 +3,6 @@
  */
 export const config = {
   secretKey: process.env.NEXTAUTH_SECRET as string,
-  googleClientId: process.env.GOOGLE_CLIENT_ID!,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 };
